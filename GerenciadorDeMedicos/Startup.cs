7d92f08de78530a80d7b9cc62a53b9b6c74481a1
Hbncod <hbncod@gmail.com>
2020-08-16 16:53:10 -0300

    Adição do OpenApi/Swagger

--- GerenciadorDeMedicos/Startup.cs
+++ GerenciadorDeMedicos/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 namespace GerenciadorDeMedicos
 {
@@ -47,6 +48,18 @@ namespace GerenciadorDeMedicos
                         ValidAudience = "GerenciadorMedicos.API"
                     };
                 });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Gerenciador de médicos",
+                    Description = "Api usada para gerenciamento de médicos.",
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -55,6 +68,12 @@ namespace GerenciadorDeMedicos
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseAuthentication();
 
             app.UseRouting();
