6ded98803baaec345ff8204eea5c8194f49dd1b8
Hbncod <hbncod@gmail.com>
2020-08-15 22:32:00 -0300

    Criação do LoginViewModel

---
+++ GerenciadorDeMedicos/ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeMedicos.ViewModel
+{
+    public class LoginViewModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "O E-mail do usuario é obrigatório")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "A senha do usuario é obrigatória")]
+        public string Senha { get; set; }
+    }
+}
